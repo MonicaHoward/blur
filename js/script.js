3b7d62d70b7014f51d5da73fcff23876ddb84785
Monica Howard <jay.m.howard@gmail.com>
2017-10-31 12:03:11 -0500

    First four sections complete

---
+++ js/script.js
@@ -0,0 +1,7 @@
+(function () {
+    $(window).scroll(function () {
+        var oVal;
+        oVal = $(blur).scrollTop() /10;
+        return $('.blur').css('opacity', oVal);
+    });
+}.call(this));
