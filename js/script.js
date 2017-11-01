d84c419f80b2efbc9c12dfca57d5e69942ee23d5
Monica Howard <jay.m.howard@gmail.com>
2017-10-31 20:38:56 -0500

    final version 1

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@
         return $('.blur').css('opacity', oVal);
     });
 }.call(this));
+
+var whichImage = "clear";
+
+function change() {
+  var image = document.getElementById('clear');
+  if(whichImage == 'clear') {
+    image.src = 'blur.png';
+    whichImage = 'blur';
+  }
+  else {
+    image.src = 'clear.png';
+    whichImage = 'clear';
+  }
+}
+
+setInterval('change()', 1000);
