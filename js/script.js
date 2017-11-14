55aac86dcf0f736ce01cbeb41243b57087628705
Monica Howard <jay.m.howard@gmail.com>
2017-11-13 22:08:40 -0600

    added animation classes but library cdn is commented out

--- js/script.js
+++ js/script.js
@@ -22,3 +22,20 @@ function change() {
   }
 }
 setInterval('change()', 1000);
+
+// CHANGE IMAGE ON SCROLL
+var onScrollHandler = function() {
+  var newImageUrl = device.png;
+  var scrollTop = document.getElementbyId('device').scrollTop || document.body.scrollTop;
+  if (scrollTop > 100) {
+     newImageUrl = "apple.png"
+  }
+  if (scrollTop > 200) {
+     newImageUrl = "blur.png"
+  }
+  if (scrollTop > 300) {
+     device.png = "clear.png"
+  }
+  document.getElementById("device")device.png = newImageUrl;
+};
+object.addEventListener ("scroll", onScrollHandler);
